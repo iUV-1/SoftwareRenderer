766ce510f84fb2fc1c40ca9f863b7866cfb40b3e
Nguyễn Minh Quyền <megaluigi2804@gmail.com>
2024-11-14 14:08:56 -0500

    cleanup and basic light setup

--- main.cpp
+++ main.cpp
@@ -20,52 +20,27 @@ const int height = 800;
 
 int main(int argc, char** argv) {
     model = new Model("obj/african_head.obj");
-    //TGAImage image(width, height, TGAImage::RGB);
-    // for (int i=0; i < model->nfaces(); i++) {
-    //     std::vector<int> face = model->face(i);
-    //     for (int j=0; j<3; j++) {
-    //         Vec3f v0 = model->vert(face[j]);
-    //         Vec3f v1 = model->vert(face[(j+1)%3]);
-    //         int x0 = (v0.x+1.)*width/2.;
-    //         int y0 = (v0.y+1.)*height/2.;
-    //         int x1 = (v1.x+1.)*width/2.;
-    //         int y1 = (v1.y+1.)*height/2.;
-    //         line(x0, y0, x1, y1, image, white);
-    //     }
-    // }
-    // Vec2i t0[3] = {Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80)};
-    // Vec2i t1[3] = {Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180)};
-    // Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
-    // triangle(t0[0], t0[1], t0[2], image, red);
-    // triangle(t1[0], t1[1], t1[2], image, white);
-    // triangle(t2[0], t2[1], t2[2], image, green);
-    // for (int i=0; i < model->nfaces(); i++) {
-    //     std::vector<int> face = model->face(i);
-    //     for (int j=0; j<3; j++) {
-    //         Vec3f v0 = model->vert(face[j]);
-    //         Vec3f v1 = model->vert(face[(j+1)%3]);
-    //         int x0 = (v0.x+1.)*width/2.;
-    //         int y0 = (v0.y+1.)*height/2.;
-    //         int x1 = (v1.x+1.)*width/2.;
-    //         int y1 = (v1.y+1.)*height/2.;
-    //         line(x0, y0, x1, y1, image, white);
-    //     }
-    // }
     TGAImage frame(1024, 1024, TGAImage::RGB);
-    // Vec2i t0[3] = {Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80)};
-    // Vec2i t1[3] = {Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180)};
-    // Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
-    // triangle(t0, frame, red);
-    // triangle(t1, frame, white);
-    // triangle(t2, frame, green);
+
+    Vec3f light(0,0, -1);
     for (int i=0; i<model->nfaces(); i++) { 
         std::vector<int> face = model->face(i); 
-        Vec2i screen_coords[3]; 
+        Vec2i screen_coords[3];
+        Vec3f world_coords[3];
         for (int j=0; j<3; j++) { 
-            Vec3f world_coords = model->vert(face[j]); 
-            screen_coords[j] = Vec2i((world_coords.x+1.)*width/2., (world_coords.y+1.)*height/2.); 
-        } 
-        triangle(screen_coords, frame, TGAColor(rand()%255, rand()%255, rand()%255, 255)); 
+            Vec3f v = model->vert(face[j]);
+            screen_coords[j] = Vec2i((v.x+1.)*width/2., (v.y+1.)*height/2.);
+            world_coords[j] = v;
+        }
+        // calculate normal
+        Vec3f n = (world_coords[2]-world_coords[0])^(world_coords[1]-world_coords[0]);
+        n.normalize();
+        // calculate light intensity by cross product between normal and light vector
+        float intensity = n*light;
+        // back face culling
+        if (intensity>0) {
+            triangle(screen_coords, frame, TGAColor(intensity*255, intensity*255, intensity*255, 255));
+        }
     }
     frame.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     frame.write_tga_file("./output.tga");
@@ -73,22 +48,6 @@ int main(int argc, char** argv) {
     return 0;
 }
 
-//void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
-//    for (float t=0.; t<1.; t+=.01) {
-//        int x = x0 + (x1-x0)*t;
-//        int y = y0 + (y1-y0)*t;
-//        image.set(x, y, color);
-//    }
-//}
-
-//void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
-//    for (float x=x0; x<=x1; x++) {
-//        float t = (x-x0)/(float)(x1-x0);
-//        int y = y0*(1. - t) + y1*t;
-//
-//        image.set(x, y, color);
-//    }
-//}
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     bool steep = false;
@@ -166,50 +125,6 @@ void line(Vec2<int> t0, Vec2<int> t1, TGAImage &image, TGAColor color) {
         }
     }
 }
-
-/*
-void triangle(Vec2<int> t0, Vec2<int> t1, Vec2<int> t2, TGAImage &image, TGAColor color) {
-    // sort the verticies by order of increasing height, t0, t1, t2
-    if (t0.y > t1.y) std::swap(t0, t1);
-    if (t0.y > t2.y) std::swap(t0, t2);
-    if (t1.y > t2.y) std::swap(t1, t2);
-    int total_height = t2.y - t0.y;
-    int segment_height = t1.y - t0.y + 1;
-    // Loop over a "scanline" of a triangle
-    // First half
-    for (int y=t0.y; y<=t1.y; y++) {
-        // Interpolation
-        float alpha = (float)(y - t0.y) / total_height;
-        float beta = (float)(y - t0.y) / segment_height;
-        // Calculate the left and right boundaries of the triangle
-        Vec2i A = t0 + (t2-t0)*alpha;
-        Vec2i B = t0 + (t1-t0)*beta;
-        // Ensure going from left to right
-        if (A.x > B.x) std::swap(A, B);
-        // Go over each point between the boundaries and fill it
-        for (int j=A.x; j <= B.x; j++) {
-            image.set(j, y, color);
-        }
-    }
-
-    // The second half
-    segment_height = t2.y - t1.y + 1;
-    for (int y=t1.y; y<=t2.y; y++) {
-        // Interpolation
-        float alpha = (float)(y - t0.y) / total_height;
-        float beta = (float)(y - t1.y) / segment_height;
-        // Calculate the left and right boundaries of the triangle
-        Vec2i A = t0 + (t2-t0)*alpha;
-        Vec2i B = t1 + (t2-t1)*beta;
-        // Ensure going from left to right
-        if (A.x > B.x) std::swap(A, B);
-        // Go over each point between the boundaries and fill it
-        for (int j=A.x; j <= B.x; j++) {
-            image.set(j, y, color); // due to int cast, t0.y+1 != A.y
-        }
-    }
-}
-*/
 
 Vec3f barycentric(Vec2i *pts, Vec2i P) {
     Vec3f u = Vec3f(pts[2].u-pts[0].u, pts[1].u-pts[0].u, pts[0].u-P.u)
